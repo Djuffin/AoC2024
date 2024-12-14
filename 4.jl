4a8a0185b6eba09b4fa644227c511156002db49b
Eugene Zemtsov <eugene@chromium.org>
2024-12-14 00:10:53 -0800

    problem 4. part 2

--- 4.jl
+++ 4.jl
@@ -7,32 +7,26 @@ for line in readlines("input4.txt")
 end
 height = length(rows)
 
-function in_boud(x, y)
-    x > 0 && y > 0 && x <= width && y <= height
+function is_x(x, y)
+    rows[y][x] == 'A' && 
+        (
+            (rows[y - 1][x - 1] == 'M' && rows[y + 1][x + 1] == 'S') ||
+            (rows[y - 1][x - 1] == 'S' && rows[y + 1][x + 1] == 'M')
+        ) &&
+        (
+            (rows[y - 1][x + 1] == 'M' && rows[y + 1][x - 1] == 'S') ||
+            (rows[y - 1][x + 1] == 'S' && rows[y + 1][x - 1] == 'M')
+        )
+    
 end
 
 word = ['X', 'M', 'A', 'S']
 result = 0
 
-directions = [ (1,0), (1,1), (0,1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1) ]
-for y in 1:height
-    for x in 1:width
-
-        for (dx, dy) in directions
-            nx = x
-            ny = y
-    
-            for letter in word
-                if !in_boud(nx,ny) || letter != rows[ny][nx]
-                    break
-                end
-                nx += dx
-                ny += dy
-
-                if letter == 'S'
-                    global result += 1
-                end
-            end
+for y in 2:(height - 1)
+    for x in 2:(width - 1)
+        if is_x(x, y)
+            global result += 1
         end
     end
 end
