ee2c8254f3a75bd2c290f4d8aaf7ec152fefd5a6
Eugene Zemtsov <eugene@chromium.org>
2024-12-14 17:01:41 -0800

    problem 6. part 2

--- 6.jl
+++ 6.jl
@@ -18,20 +18,20 @@ dir_change = Dict(
     (-1, 0) => (0, -1)
 )
 
-function main()
-    y,x = Tuple(findfirst(isequal('^'), m))
-    dx = 0
-    dy = -1
-    path = Set()
-    function is_in(x, y)
-        x > 0 && y > 0 && x <= width && y <= height
-    end
-    function obstacle(x, y)
-        is_in(x, y) && m[y,x] == '#'
-    end
+function is_in(x, y)
+    x > 0 && y > 0 && x <= width && y <= height
+end
+function obstacle(x, y)
+    is_in(x, y) && m[y,x] == '#'
+end
 
+function find_way_out(x, y, dx, dy)
+    path = Set()
     while is_in(x, y)
-        push!(path, (x,y))
+        if (x,y, dx, dy) in path
+            return nothing
+        end
+        push!(path, (x,y, dx, dy))
         nx, ny = x + dx, y + dy
         while obstacle(nx, ny)
             dx, dy = dir_change[(dx,dy)]
@@ -40,6 +40,23 @@ function main()
         x,y = nx,ny
     end
     path
+end
+
+function main()
+    y,x = Tuple(findfirst(isequal('^'), m))
+    sx, sy = x,y
+    path = find_way_out(sx, sy, 0, -1)
+    obstacles = Set()
+    for (x, y, dx, dy) in path
+        if !obstacle(x, y) && is_in(x, y) && (x,y) != (sx, sy)
+            m[y, x] = '#'
+            if isnothing(find_way_out(sx, sy, 0, -1))
+                push!(obstacles, (x, y))
+            end
+            m[y, x] = '.'
+        end
+    end
+    obstacles
 end
 
 print(length(main()))
